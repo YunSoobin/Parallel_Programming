b6e0adc7f52f415c72422ebdd5c12712c826065c
YunSoobin <Soobin5296@gmail.com>
2023-06-07 19:08:10 +0900

    2. 영상에 Salt and Pepper Noise 추가

--- Parallel Programming/main.cpp
+++ Parallel Programming/main.cpp
@@ -3,6 +3,8 @@
 using namespace std;
 using namespace cv;
 
+void addNoise(Mat &image, int noise_num);
+
 int main(void)
 {
     VideoCapture open_origin_video("vtest.avi");
@@ -19,7 +21,8 @@ int main(void)
     int fourcc = VideoWriter::fourcc('m', 'p', '4', 'v');
     
     VideoWriter save_origin_video("save_origin_video.mov", fourcc, fps, Size(width, height));
-
+    VideoWriter save_noise_video("save_noise_video.mov", fourcc, fps, Size(width, height));
+    
     while(open_origin_video.isOpened())
     {
         Mat origin_frame;
@@ -34,6 +37,16 @@ int main(void)
         // 원본 영상 출력, 저장
         imshow("Origin Video", origin_frame);
         save_origin_video << origin_frame;
+
+
+        Mat noise_frame;
+        noise_frame = origin_frame.clone();
+        
+        addNoise(noise_frame, 5000);    // 노이즈 추가
+
+        // 노이즈가 적용된 영상 출력, 저장
+        imshow("Noise Video", noise_frame);
+        save_noise_video << noise_frame;
         
         
         if(waitKey(1000 / fps) == 'q')
@@ -44,9 +57,35 @@ int main(void)
     
     open_origin_video.release();
     save_origin_video.release();
+    save_noise_video.release();
     
     
     destroyAllWindows();
     
     return 0;
+}
+
+
+void addNoise(Mat &image, int noise_num)
+{
+    srand((unsigned int)time(NULL));
+    
+    for(int k = 0; k < noise_num; k++)
+    {
+        int col_pixel = rand() % image.cols;    // noise를 추가할 열을 임의로 선택
+        int row_pixel = rand() % image.rows;    // noise를 추가할 행을 임의로 선택
+        int ch_num = image.channels();
+        int noise_color = (rand() % 2) * 255;   // 0 or 255
+        
+        if(ch_num == 1) // 흑백 영상
+        {
+            image.at<uchar>(row_pixel, col_pixel) = noise_color;    // 임의 픽셀의 픽셀값을 검정색(0) 혹은 흰색(255)으로 교체
+        }
+        else if(ch_num == 3)    // 컬러 영상
+        {
+            image.at<Vec3b>(row_pixel, col_pixel)[0] = noise_color;  // B 채널
+            image.at<Vec3b>(row_pixel, col_pixel)[1] = noise_color;  // G 채널
+            image.at<Vec3b>(row_pixel, col_pixel)[2] = noise_color;  // R 채널
+        }
+    }
 }
