6ed6a2f7412dc5457694e01c754de07b967e3728
YunSoobin <Soobin5296@gmail.com>
2023-06-13 13:36:56 +0900

    3. 영상에 Median Filter(중간값 필터) 적용

--- Parallel Programming/main.cpp
+++ Parallel Programming/main.cpp
@@ -3,7 +3,9 @@
 using namespace std;
 using namespace cv;
 
-void addNoise(Mat &image, int noise_num);
+void addNoise(Mat &image, int noise_num);   // 노이즈 추가
+void applyFilter(Mat &image, int kernal_size);  // 필터 적용
+void Bubble_Sort(vector<int> &pixels);   // 버블 정렬
 
 int main(void)
 {
@@ -22,6 +24,7 @@ int main(void)
     
     VideoWriter save_origin_video("save_origin_video.mov", fourcc, fps, Size(width, height));
     VideoWriter save_noise_video("save_noise_video.mov", fourcc, fps, Size(width, height));
+    VideoWriter save_filter_video("save_filter_video.mov", fourcc, fps, Size(width, height));
     
     while(open_origin_video.isOpened())
     {
@@ -49,7 +52,17 @@ int main(void)
         save_noise_video << noise_frame;
         
         
-        if(waitKey(1000 / fps) == 'q')
+        Mat filter_frame;
+        filter_frame = noise_frame.clone();
+        
+        applyFilter(filter_frame, 3);  // 필터 적용
+        
+        // 필터가 적용된 영상 출력, 저장
+        imshow("Filter Video", filter_frame);
+        save_filter_video << filter_frame;
+        
+
+        if(waitKey(1000) == 'q')
         {
             break;
         }
@@ -58,6 +71,7 @@ int main(void)
     open_origin_video.release();
     save_origin_video.release();
     save_noise_video.release();
+    save_filter_video.release();
     
     
     destroyAllWindows();
@@ -86,6 +100,84 @@ void addNoise(Mat &image, int noise_num)
             image.at<Vec3b>(row_pixel, col_pixel)[0] = noise_color;  // B 채널
             image.at<Vec3b>(row_pixel, col_pixel)[1] = noise_color;  // G 채널
             image.at<Vec3b>(row_pixel, col_pixel)[2] = noise_color;  // R 채널
+        }
+    }
+}
+
+void applyFilter(Mat &image, int kernal_size)
+{
+    int row_size = image.rows;
+    int col_size = image.cols;
+    int ch_num = image.channels();
+    
+    int center_pixel = kernal_size / 2;
+    
+    if(ch_num == 1) // 흑백 영상
+    {
+        vector<int> pixels(kernal_size * kernal_size);
+        for(int row = 0; row < row_size - kernal_size; row++)
+        {
+            for(int col = 0; col < col_size - kernal_size; col++)
+            {
+                pixels.clear();
+                for(int r = 0; r < kernal_size; r++)
+                {
+                    for(int c = 0; c < kernal_size; c++)
+                    {
+                        pixels.push_back(image.at<uchar>(row + r, col + c));
+                    }
+                }
+                
+                Bubble_Sort(pixels);
+                
+                image.at<uchar>(row + center_pixel, col + center_pixel) = pixels.at(kernal_size + center_pixel);
+            }
+        }
+    }
+    else if(ch_num == 3)   // 컬러 영상
+    {
+        vector<int> pixels_B(kernal_size * kernal_size);
+        vector<int> pixels_G(kernal_size * kernal_size);
+        vector<int> pixels_R(kernal_size * kernal_size);
+        for(int row = 0; row < row_size - kernal_size; row++)
+        {
+            for(int col = 0; col < col_size - kernal_size; col++)
+            {
+                pixels_B.clear();
+                pixels_R.clear();
+                pixels_G.clear();
+                for(int r = 0; r < kernal_size; r++)
+                {
+                    for(int c = 0; c < kernal_size; c++)
+                    {
+                        pixels_B.push_back(image.at<Vec3b>(row + r, col + c)[0]);
+                        pixels_G.push_back(image.at<Vec3b>(row + r, col + c)[1]);
+                        pixels_R.push_back(image.at<Vec3b>(row + r, col + c)[2]);
+                    }
+                }
+                
+                Bubble_Sort(pixels_B);
+                Bubble_Sort(pixels_G);
+                Bubble_Sort(pixels_R);
+                
+                image.at<Vec3b>(row + center_pixel, col + center_pixel)[0] = pixels_B.at(kernal_size + center_pixel);
+                image.at<Vec3b>(row + center_pixel, col + center_pixel)[1] = pixels_G.at(kernal_size + center_pixel);
+                image.at<Vec3b>(row + center_pixel, col + center_pixel)[2] = pixels_R.at(kernal_size + center_pixel);
+            }
+        }
+    }
+}
+
+void Bubble_Sort(vector<int> &pixels)
+{
+    for(int i = 0; i < pixels.size() - 1; i++)
+    {
+        for(int j = 0; j < pixels.size() - 1; j++)
+        {
+            if(pixels[j] > pixels[j + 1])
+            {
+                swap(pixels[j], pixels[j + 1]);
+            }
         }
     }
 }
